f01829524db7e94d444a64a42611855ea5a2aafc
Kaushal <bhtkaushal643@gmail.com>
2026-09-02 04:36:20 +0530

    feat(PluginProcessor): implement parameters and CMakeLists

---
+++ source/PluginEditor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit AudioPluginAudioProcessorEditor (SimpleEQProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    SimpleEQProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
